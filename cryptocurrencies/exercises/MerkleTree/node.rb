d152adabdfd2db65b69ab3471171b9eb2d851d99
viceversus <kenshimizu88@gmail.com>
2018-02-18 00:19:01 -0800

    Proofs kind of working

--- cryptocurrencies/exercises/MerkleTree/node.rb
+++ cryptocurrencies/exercises/MerkleTree/node.rb
@@ -1,10 +1,12 @@
 require 'Digest'
+require 'byebug'
 
 module MerkleTree
   class Node
-    attr_reader :value, :left, :right
+    attr_reader :header, :value, :left, :right
 
-    def initialize(val, left=nil, right=nil)
+    def initialize(header, val=nil, left=nil, right=nil)
+      @header = header
       @value = val
       @left = left
       @right = right
@@ -14,32 +16,60 @@ module MerkleTree
       @left == nil && @right == nil
     end
 
+    def generate_proof(header, proof = [])
+      return if is_leaf?
+
+      children = [@left.header, @right.header]
+      if children.include? header
+        proof += children
+      else
+        children_proofs = [@left.generate_proof(header, proof), @right.generate_proof(header, proof)].flatten.compact
+        unless children_proofs.empty?
+          children_proofs + children
+        end
+      end
+    end
+
     class << self
       def create_from(data)
         nodes = []
         nodeify_data(data).each_slice(2) do |node_pair|
           add_padding(node_pair)
-          string_pair = "#{value_from(node_pair[0])}||#{value_from(node_pair[1])}"
-          val = Digest::SHA2.hexdigest(string_pair)
-          nodes << Node.new(val, node_pair[0], node_pair[1])
+          header = Digest::SHA2.hexdigest("#{node_pair[0].header}||#{node_pair[1].header}")
+          nodes << Node.new(header, nil, node_pair[0], node_pair[1])
         end
 
         nodes.length > 1 ? create_from(nodes) : nodes[0]
       end
 
-      private
-      def value_from(node)
-        node.is_leaf? ? Digest::SHA2.hexdigest(node.value) : node.value
+      def verify_inclusion(block, proof, root)
+        return false unless proof.include?(block) && proof.length > 1
+        index = proof.find_index(block)
+        next_hash = if index == 0
+          Digest::SHA2.hexdigest("#{block}||#{proof[1]}")
+        else
+          Digest::SHA2.hexdigest("#{proof[0]}||#{block}")
+        end
+
+        return true if next_hash == root
+        verify_inclusion(next_hash, proof[2..-1], root)
       end
 
+      private
       def add_padding(node_pair)
         if node_pair.length == 1
-          node_pair[0].is_leaf? ? node_pair.push(node_pair[0]) : node_pair.push(Node.new(''))
+          node_pair[0].is_leaf? ? node_pair.push(Node.new('')) : node_pair.push(node_pair[0])
         end
       end
 
       def nodeify_data(data)
-        data.map { |datum| datum.is_a?(MerkleTree::Node) ? datum : Node.new(datum) }
+        data.map do |datum|
+          if datum.is_a?(MerkleTree::Node)
+            datum
+          else
+            Node.new(Digest::SHA2.hexdigest(datum), datum)
+          end
+        end
       end
     end
   end
--- cryptocurrencies/exercises/MerkleTree/node_spec.rb
+++ cryptocurrencies/exercises/MerkleTree/node_spec.rb
@@ -6,7 +6,7 @@ describe MerkleTree::Node do
     it "returns the correct hash" do
       data = %w(We hold these truths to be self-evident that)
       root = MerkleTree::Node.create_from(data)
-      expect(root.value).to eq "4a359c93d6b6c9beaa3fe8d8e68935aa5b5081bd2603549af88dee298fbfdd0a"
+      expect(root.header).to eq "4a359c93d6b6c9beaa3fe8d8e68935aa5b5081bd2603549af88dee298fbfdd0a"
     end
 
     it "creates hash when there are odd numbers of words" do
@@ -17,10 +17,62 @@ describe MerkleTree::Node do
     it "uses a different padding strategy for internal and leaf nodes" do
       data = %w(We hold these truths to)
       root = MerkleTree::Node.create_from(data)
-      leaf_padding = root.right.left.right.value
-      internal_padding = root.right.right.value
+      leaf_padding = root.right.left.right.header
+      internal_padding = root.right.right.header
 
       expect(leaf_padding).not_to eq internal_padding
+    end
+  end
+
+  describe '#generate_proof' do
+    let(:tree) { MerkleTree::Node.create_from(data) }
+
+    context 'simple tree' do
+      let(:data) { %w(A B C) }
+
+      it "returns all relevant hashes for a block" do
+        expect(
+          tree.generate_proof(tree.right.header)
+        ).to eq([tree.left.header, tree.right.header])
+      end
+    end
+
+    context 'large tree' do
+      let(:data) { %w(A B C D E F G H) }
+
+      it "returns all relevant hashes for a block" do
+        expect(
+          tree.generate_proof(tree.right.left.left.header)
+        ).to eq([
+          tree.right.left.left.header,
+          tree.right.left.right.header,
+          tree.right.left.header,
+          tree.right.right.header,
+          tree.left.header,
+          tree.right.header
+        ])
+      end
+    end
+  end
+
+  describe '.verify_inclusion' do
+    let(:tree) { MerkleTree::Node.create_from(data) }
+    let(:data) { %w(A B C D E F G H) }
+    let(:proof) { [
+      tree.right.left.left.header,
+      tree.right.left.right.header,
+      tree.right.left.header,
+      tree.right.right.header,
+      tree.left.header,
+      tree.right.header
+    ] }
+
+    it "returns true when proof is accurate" do
+      expect(MerkleTree::Node.verify_inclusion(tree.right.left.left.header, proof, tree.header)).to eq(true)
+    end
+
+    it "returns false when proof is inaccurate" do
+      expect(MerkleTree::Node.verify_inclusion(tree.right.left.left.header, proof, tree.left.left.header)).to eq(false)
     end
   end
 end

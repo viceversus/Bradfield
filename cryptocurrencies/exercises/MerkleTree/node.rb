21f6c9e9d0b4ab3d011590be2db2277616123230
viceversus <kenshimizu88@gmail.com>
2018-02-17 19:58:00 -0800

    Different padding strategy for leaf vs internal

--- cryptocurrencies/exercises/MerkleTree/node.rb
+++ cryptocurrencies/exercises/MerkleTree/node.rb
@@ -4,30 +4,42 @@ module MerkleTree
   class Node
     attr_reader :value, :left, :right
 
-    def initialize(val, left, right)
+    def initialize(val, left=nil, right=nil)
       @value = val
       @left = left
       @right = right
     end
 
-    def self.create_from(data)
-      nodes = []
-      data.push '' if data.length.odd?
-      data.each_slice(2) do |node_pair|
-        string_pair = "#{value_from(node_pair[0])}||#{value_from(node_pair[1])}"
-        val = Digest::SHA2.hexdigest(string_pair)
-        nodes << Node.new(val, node_pair[0], node_pair[1])
-      end
-
-      nodes.length > 1 ? create_from(nodes) : nodes[0]
+    def is_leaf?
+      @left == nil && @right == nil
     end
 
-    private
-    def self.value_from(node)
-      if node.respond_to? :value
-        node.value
-      else
-        Digest::SHA2.hexdigest(node)
+    class << self
+      def create_from(data)
+        nodes = []
+        nodeify_data(data).each_slice(2) do |node_pair|
+          add_padding(node_pair)
+          string_pair = "#{value_from(node_pair[0])}||#{value_from(node_pair[1])}"
+          val = Digest::SHA2.hexdigest(string_pair)
+          nodes << Node.new(val, node_pair[0], node_pair[1])
+        end
+
+        nodes.length > 1 ? create_from(nodes) : nodes[0]
+      end
+
+      private
+      def value_from(node)
+        node.is_leaf? ? Digest::SHA2.hexdigest(node.value) : node.value
+      end
+
+      def add_padding(node_pair)
+        if node_pair.length == 1
+          node_pair[0].is_leaf? ? node_pair.push(node_pair[0]) : node_pair.push(Node.new(''))
+        end
+      end
+
+      def nodeify_data(data)
+        data.map { |datum| datum.is_a?(MerkleTree::Node) ? datum : Node.new(datum) }
       end
     end
   end
--- cryptocurrencies/exercises/MerkleTree/node_spec.rb
+++ cryptocurrencies/exercises/MerkleTree/node_spec.rb
@@ -5,13 +5,22 @@ describe MerkleTree::Node do
   describe '.create_from' do
     it "returns the correct hash" do
       data = %w(We hold these truths to be self-evident that)
-      node = MerkleTree::Node.create_from(data)
-      expect(node.value).to eq "4a359c93d6b6c9beaa3fe8d8e68935aa5b5081bd2603549af88dee298fbfdd0a"
+      root = MerkleTree::Node.create_from(data)
+      expect(root.value).to eq "4a359c93d6b6c9beaa3fe8d8e68935aa5b5081bd2603549af88dee298fbfdd0a"
     end
 
     it "creates hash when there are odd numbers of words" do
       data = %w(We hold these truths to be self-evident)
       expect { MerkleTree::Node.create_from(data) }.to_not raise_error
+    end
+
+    it "uses a different padding strategy for internal and leaf nodes" do
+      data = %w(We hold these truths to)
+      root = MerkleTree::Node.create_from(data)
+      leaf_padding = root.right.left.right.value
+      internal_padding = root.right.right.value
+
+      expect(leaf_padding).not_to eq internal_padding
     end
   end
 end
